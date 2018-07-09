a766698de41e5b5d6e2452d4d92a94ba41de9649
Michael Keyser <erutan409@hotmail.com>
2018-07-08 21:11:54 -0400

    Removing VS files
    Updated .gitignore

--- examples/onoffbutton/Release/Adafruit-GFX-Library/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\eruta\AppData\Local\Temp\VMBuilds\onoffbutton\mega_atmega2560\Release\Adafruit-GFX-Library\glcdfont.c.o: \
- C:\Users\eruta\Documents\Arduino\libraries\Adafruit-GFX-Library\glcdfont.c
